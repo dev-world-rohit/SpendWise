50d3c24af40b8eea91d42c688616790759acab0f
dev_world_rohit <aniationcreatives@gmail.com>
2024-06-05 12:30:20 +0530

    updated

--- src/context/Authenticatio.jsx
+++ src/context/Authenticatio.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-    const url = "http://127.0.0.1:5000";
+    const url = "https://following-matty-anaition-af103e26.koyeb.app/" // "http://127.0.0.1:5000";
 
     function getToken() {
         const userToken = localStorage.getItem("token");
